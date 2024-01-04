a53001b1dc0fbe38aceec64e8d5d1822d63fa137
aditya0by0 <aditya0by0@gmail.com>
2024-01-04 01:05:53 +0100

    Highchart integration for packed bubble charts
    - Chart.js doesn't have packed bubble charts to visualize clusters
    - Only Highcharts have packed bubble chart

--- frontend/react-search-app/src/components/BubbleChart.tsx
+++ frontend/react-search-app/src/components/BubbleChart.tsx
@@ -1,64 +1,61 @@
 import React, { useEffect, useRef } from "react";
-import Chart from "chart.js/auto";
-import { Document, ClusteredData } from "./FetchedDataInterface";
+import { ClusteredData } from "./FetchedDataInterface";
+import Highcharts from "highcharts";
+import HC_more from "highcharts/highcharts-more";
+import HighchartsReact from "highcharts-react-official";
+HC_more(Highcharts);
 
 interface BubbleChartProps {
   data: ClusteredData | undefined;
 }
 
 const BubbleChart: React.FC<BubbleChartProps> = ({ data }) => {
-  const chartRef = useRef<HTMLCanvasElement | null>(null);
+  const chartRef = useRef<Highcharts.Chart | null>(null);
+  const bubblechartOptions: any = {
+    chart: {
+      type: "packedbubble",
+    },
+    series: undefined,
+    plotOptions: {
+      packedbubble: {
+        layoutAlgorithm: {
+          gravitationalConstant: 0.05,
+          splitSeries: true,
+          seriesInteraction: false,
+          dragBetweenSeries: true,
+          parentNodeLimit: true,
+        },
+      },
+    },
+    tooltip: {
+      useHTML: true,
+      pointFormat: `<b>Title:</b> {point.docName}<br>
+                    <b>Relevance score:</b> {point.score}<br>
+                    <b>Content:</b> {point.docExtract}`,
+    },
+    title: {
+      text: "Search Result Clusters",
+    },
+  };
 
   useEffect(() => {
     if (chartRef.current && data) {
-      const ctx = chartRef.current.getContext("2d");
+      // Update series data
+      bubblechartOptions.series = data;
 
-      if (ctx) {
-        // Aggregate data by clusters
-        const clusters = Object.values(data).map((cluster) => ({
-          x: cluster.length, // x-axis: number of documents in the cluster
-          y: cluster[0].score, // y-axis: average score of the cluster (adjust as needed)
-          r: cluster.length * 5, // bubble size: proportional to the number of documents
-        }));
-
-        new Chart(ctx, {
-          type: "bubble",
-          data: {
-            datasets: [
-              {
-                label: "Clusters",
-                data: clusters,
-                backgroundColor: "rgba(75, 192, 192, 0.2)",
-                borderColor: "rgba(75, 192, 192, 1)",
-              },
-            ],
-          },
-          options: {
-            scales: {
-              x: {
-                type: "linear",
-                position: "bottom",
-                title: {
-                  display: true,
-                  text: "Number of Documents in Cluster",
-                },
-              },
-              y: {
-                type: "linear",
-                position: "left",
-                title: {
-                  display: true,
-                  text: "Average Score of Cluster",
-                },
-              },
-            },
-          },
-        });
-      }
+      // Create or update the chart
+      chartRef.current = Highcharts.chart(
+        chartRef.current as any,
+        bubblechartOptions
+      );
     }
-  }, [data]);
+  }, [data, bubblechartOptions]);
 
-  return <canvas ref={chartRef} />;
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={bubblechartOptions} />
+    </div>
+  );
 };
 
 export default BubbleChart;
--- frontend/react-search-app/src/components/RightPanel.tsx
+++ frontend/react-search-app/src/components/RightPanel.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./RightPanel.module.css";
+import { useData } from "./DataContext";
+import BubbleChart from "./BubbleChart";
 
 const RightPanel: React.FC = () => {
+  const { fetchedData } = useData();
+
   return (
     <div className={styles.rightPanel}>
-      <h2>Results - Clustering</h2>
-      {/* Add your content for the right panel */}
+      {/* <h2>Results - Clustering</h2> */}
+      <BubbleChart data={fetchedData} />
     </div>
   );
 };
