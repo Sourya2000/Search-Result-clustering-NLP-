48448274c5bc06e8442f878bea67b04cb86712c3
aditya0by0 <aditya0by0@gmail.com>
2024-01-10 22:09:04 +0100

    added Optimal k + Feature vector + minor bug fix

--- backend/src/main/java/com/Constants/Constants.java
+++ backend/src/main/java/com/Constants/Constants.java
@@ -22,8 +22,15 @@ public class Constants {
 	public static final String INPUT_SEARCH_QUERY_STRING = "searchString";
 
 	public static final String CLUSTERING_ALGO_KMEANS_STRING = "kMeans";
+	public static final int CLUSTERING_KMEANS_MIN_CLUSTERS = 50;
 
 	// Custom Exceptions
 	public static final String ALOG_NOT_IN_CONFIG_STRING = "Algorithm not present in the configuration";
 	public static final String REQUEST_INPUT_PARAM_NOT_CORRECT_STRING = "Input Parameters not correct/sufficent to proceed";
+
+	// Custom prints
+	public static final String PREV_INDEXED_FILES_CLEARED_STRING = "Previously Indexed files in Index Directory cleared successfully.";
+	public static final String SERVER_START_INDEXING_START_STRING = "Server is started. Indexing the Document Collection....";
+	public static final String INDEXING_COMPLETED_STRING = "Indexing completed";
+	public static final String DOC_FEATURE_VECTOR_SIZE_STRING = "Size of the vectorized document by TF-IDF weighting scheme: ";
 }
